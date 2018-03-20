23030a21170a99fdffaebe9952113027174563b5
Phillip Longman <himself@philliplongman.com>
2018-03-20 02:16:15 -0400

    Initial commit.

---
+++ spec/sinatra-cli_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe SinatraCli do
+  it "has a version number" do
+    expect(SinatraCli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
