bac3ae4fe56a3ed6a925750c31787041c3b2d074
Phillip Longman <himself@philliplongman.com>
2018-03-29 16:22:18 -0400

    Report failure to parent process.

--- lib/sinatra-cli/cli.rb
+++ lib/sinatra-cli/cli.rb
@@ -13,6 +13,12 @@ module SinatraCli
   class Cli < Thor
     include Thor::Actions
 
+    class << self
+      def exit_on_failure?
+        true
+      end
+    end
+
     # Import commands from discrete files
     include Commands::New
     include Commands::Generate
