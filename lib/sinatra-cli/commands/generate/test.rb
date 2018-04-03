3a0a221bd198151ec2249c82291a89333abe37f1
Phillip Longman <himself@philliplongman.com>
2018-04-02 21:45:09 -0400

    Change command name. Add documentation.

---
+++ lib/sinatra-cli/commands/generate/test.rb
@@ -0,0 +1,25 @@
+module SinatraCli
+  class Generate < Cli
+
+    desc "generate tests", "Generate an RSpec installation"
+
+    # Run the RSpec generator in the current working directory, and output
+    # instructions how to start the tests. RSpec is the only testing
+    # framework currently supported.
+    #
+    def tests
+      app = RSpec.new(cli: self).generate
+      say <<~SAY
+
+        Success! RSpec and Capybara have been installed. Put your tests in #{app.underscored_name}/spec/
+        To run your them, use the command
+
+            #{cmd :tests}
+
+      SAY
+    end
+
+    map "test" => :tests
+
+  end
+end
