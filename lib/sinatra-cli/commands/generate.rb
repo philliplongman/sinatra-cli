d148eda91e3468b395550fa8da8b7ed4d19d0eb2
Phillip Longman <himself@philliplongman.com>
2018-04-02 21:43:37 -0400

    Add documentation.

--- lib/sinatra-cli/commands/generate.rb
+++ lib/sinatra-cli/commands/generate.rb
@@ -4,6 +4,8 @@ require_rel "../main"
 module SinatraCli
   class Generate < Cli
 
+    # Register Generate as a subcommand of the Main cli class.
+    # 
     Main.class_eval do
       desc "generate ELEMENT [options]", "Run generator to add ELEMENT to project"
       subcommand "generate", Generate
