9e32ff39d18a0301eec41bc4a775c208f3ba780a
Phillip Longman <himself@philliplongman.com>
2018-04-02 23:17:25 -0400

    Add documentation. Split method.

--- lib/sinatra-cli/generators/concerns/gemable.rb
+++ lib/sinatra-cli/generators/concerns/gemable.rb
@@ -4,37 +4,73 @@ module SinatraCli
   module Generators
     module Gemable
       extend ActiveSupport::Concern
-
+      #
+      # -------------------------------------
+      # Methods for interacting with Gemfiles
+      # -------------------------------------
+      #
       included do
         private
 
+        # Relative path to the Gemfile in the app
+        #
         def gemfile
           "#{app_path}/Gemfile"
         end
 
-        def ensure_gemfile_exists
+        # Raise an error if there is no Gemfile, before trying to
+        # interact with it.
+        #
+        def check_for_gemfile
           raise Thor::Error, "No Gemfile" unless File.exist?(gemfile)
         end
 
-        def add_gems(gems, group: nil)
-          ensure_gemfile_exists
+        # Add gem(s) to the appropriate spot in the Gemfile. For simplicity,
+        # it currently accepts a plain text code that will be inserted
+        # directly into the Gemfile. This is much simpler than trying to
+        # convert Ruby code into plain text.
+        #
+        def add_gems(gems_code, group: nil)
+          check_for_gemfile
 
-          # ensure newline
-          gems = gems.chomp << "\n"
+          # ensure a newline at the end
+          gems_code = gems_code.chomp << "\n"
 
           if group
-            group_str = Array(group).join(", ").gsub(/\b(?=\w)/, ":")
-            pattern = "group #{group_str} do\n"
-            gems.gsub!(/^/, "  ")
+            add_to_group(group, gems_code)
           else
-            pattern = /(?:source).+?(?=\ngroup)/m
+            add_to_default(gems_code)
           end
-
-          cli.insert_into_file(gemfile, gems, after: pattern)
         end
 
+        # Add text at the bottom of the default group
+        #
+        def add_to_default(gems_code)
+          # starting the the top with "source", find the very last line
+          # in the default group, before the gem "group"
+          pattern = /(?:source).+?(?=\ngroup)/m
+
+          cli.insert_into_file(gemfile, gems_code, after: pattern)
+        end
+
+        # Add text to the bottom of the gem group
+        #
+        def add_to_group(group, gems_code)
+          # convert group to string formatted like `:development, :test`
+          group_top = Array(group).join(", ").gsub(/\b(?=\w)/, ":")
+          # find top of block under `group .. do` and `end`
+          pattern = "group #{group_str} do\n"
+
+          # add padding to text
+          gems_code.gsub!(/^/, "  ")
+
+          cli.insert_into_file(gemfile, gems_code, after: pattern)
+        end
+
+        # Run `bundle install` with the Gemfile for the project
+        #
         def bundle_gems
-          ensure_gemfile_exists
+          check_for_gemfile
           cli.run "BUNDLE_GEMFILE=#{gemfile} bundle install"
         end
       end
